d41d56c3daf6014a60b4528fc2ed4d6d0370733b
Vanessa <vanessaparfeni@mail.ru>
2023-12-07 20:57:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterView,RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <header>
+        <RouterLink to="/">Главная</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </header>
+    <main>
+        <RouterView />
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style  scoped>
+
+</style>
